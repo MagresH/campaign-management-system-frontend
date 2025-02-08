784fb59ddfcb3727d064faf5c1f6d7e6b64c5829
MagresH <magresh11@gmail.com>
2025-02-08 13:27:41 +0100

    Initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideHttpClient(
+      withInterceptors([
+      ])
+    ),
+    provideRouter(routes),
+  ],
+}).catch(err => console.error(err));
